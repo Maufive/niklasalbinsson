61f801347604b46d94ffb55d7d6becf58494cc42
Niklas Albinsson <albinssonniklas@gmail.com>
2022-05-28 16:49:09 +0200

    Minor spacing inconsistency fix

--- components/blog/post-list.tsx
+++ components/blog/post-list.tsx
@@ -12,7 +12,7 @@ const BlogPostList: React.FC<BlogPostListProps> = ({
   children,
 }) => (
   <section>
-    <h2 className="mb-10 text-2xl font-bold">{title}</h2>
+    <h2 className="mb-6 text-2xl font-bold">{title}</h2>
     {posts.length > 0 ? (
       <ul className="mb-6 space-y-4">
         {posts.map((post) => (
