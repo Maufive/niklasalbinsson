b0719fd42a7c28ac95d2c1e663ee8f84544923ca
Niklas Albinsson <albinssonniklas@gmail.com>
2022-08-29 21:25:07 +0200

    New styling for hover effects on blog post links

--- components/blog/post.tsx
+++ components/blog/post.tsx
@@ -6,18 +6,16 @@ import { formatDate } from 'utils/formatDate';
 const Post: React.FC<{ post: PostType }> = ({ post }) => (
   <li>
     <Link href={`/blog/${post.slug}`} passHref>
-      <a className="block w-full transform cursor-pointer rounded-xl bg-transparent from-transparent to-transparent p-1 transition-all hover:scale-[1.03] hover:bg-gradient-to-r hover:from-[#D8B4FE] hover:to-[#818CF8] focus:scale-[1.03] focus:bg-gradient-to-r focus:from-[#D8B4FE] focus:to-[#818CF8]">
-        <div className="flex h-full flex-col justify-between rounded-lg bg-zinc-100 p-2 transition-colors dark:bg-zinc-900 xl:p-6">
-          <h3 className="mb-2 text-xl font-bold">{post.title}</h3>
-          <p className="mb-2 text-base">{post.summary}</p>
-          <p className="text-sm text-zinc-600 dark:text-zinc-400">
-            Published on{' '}
-            <time dateTime={post.publishedAt}>
-              {formatDate(post.publishedAt)}
-            </time>{' '}
-            &middot; {post.readingTime.text}
-          </p>
-        </div>
+      <a className="flex h-full w-full transform cursor-pointer flex-col justify-between rounded-xl bg-zinc-100 p-6 transition-all hover:scale-[1.03] focus:scale-[1.03] dark:bg-zinc-900 dark:hover:bg-zinc-800 xl:p-6">
+        <h3 className="mb-2 text-xl font-bold">{post.title}</h3>
+        <p className="mb-2 text-base">{post.summary}</p>
+        <p className="text-sm text-zinc-600 dark:text-zinc-400">
+          Published on{' '}
+          <time dateTime={post.publishedAt}>
+            {formatDate(post.publishedAt)}
+          </time>{' '}
+          &middot; {post.readingTime.text}
+        </p>
       </a>
     </Link>
   </li>
