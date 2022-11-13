3a0f4c1f24b644ab4712a6cd602b4b0b48a3939a
Niklas Albinsson <albinssonniklas@gmail.com>
2022-11-13 13:20:53 +0100

    Tweaking desktop links styling

--- components/navigation/desktop-menu.tsx
+++ components/navigation/desktop-menu.tsx
@@ -1,8 +1,33 @@
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { useProgress } from 'utils/hooks';
+import React from 'react';
 import { LINKS } from './links';
 import ThemeSwitcher from '../theme-switcher/theme-switcher';
+
+type StyledLinksProps = {
+  children?: React.ReactNode | React.ReactNode[];
+  isActive?: boolean;
+  href: string;
+};
+
+const StyledLink: React.FC<StyledLinksProps> = ({
+  children,
+  isActive,
+  href,
+}) => (
+  <Link href={href} passHref>
+    <a
+      className={`rounded-md py-2 px-3 text-base font-normal transition-colors hover:bg-zinc-200 focus:ring focus:ring-zinc-900 dark:hover:bg-zinc-700 dark:focus:ring-zinc-200 ${
+        isActive
+          ? 'bg-zinc-200 text-zinc-900 hover:text-zinc-800 dark:bg-zinc-700 dark:text-zinc-100'
+          : 'text-zinc-600 focus:text-zinc-800 dark:text-zinc-400 dark:hover:text-zinc-200 dark:focus:text-zinc-300'
+      }`}
+    >
+      {children}
+    </a>
+  </Link>
+);
 
 const DesktopMenu: React.FC<{ pathname: string; showProgressBar: boolean }> = ({
   pathname,
@@ -22,17 +47,9 @@ const DesktopMenu: React.FC<{ pathname: string; showProgressBar: boolean }> = ({
         <nav>
           <ol className="flex py-0">
             <li className="mr-4 cursor-pointer transition-colors">
-              <Link href="/" passHref>
-                <a
-                  className={`rounded-md py-1 px-2 transition-colors hover:bg-zinc-200 focus:ring focus:ring-primary dark:hover:bg-zinc-700 ${
-                    pathname === '/'
-                      ? 'font-bold text-zinc-900 hover:text-zinc-800 dark:text-zinc-100'
-                      : 'font-normal text-zinc-600 focus:text-zinc-800 dark:text-zinc-400 dark:hover:text-zinc-200 dark:focus:text-zinc-300'
-                  }`}
-                >
-                  Home
-                </a>
-              </Link>
+              <StyledLink isActive={pathname === '/'} href="/">
+                Home
+              </StyledLink>
             </li>
 
             {LINKS.map((link) => (
@@ -40,17 +57,12 @@ const DesktopMenu: React.FC<{ pathname: string; showProgressBar: boolean }> = ({
                 key={link.href}
                 className="mr-4 cursor-pointer transition-colors"
               >
-                <Link key={link.href} href={link.href} passHref>
-                  <a
-                    className={`rounded-md px-2 py-1 transition-colors hover:bg-zinc-200 focus:ring focus:ring-primary dark:hover:bg-zinc-700 ${
-                      pathname.includes(link.href)
-                        ? 'font-bold text-zinc-900 hover:text-zinc-800 dark:text-zinc-100'
-                        : 'font-normal text-zinc-600 focus:text-zinc-800 dark:text-zinc-400 dark:hover:text-zinc-200 dark:focus:text-zinc-300'
-                    }`}
-                  >
-                    {link.label}
-                  </a>
-                </Link>
+                <StyledLink
+                  isActive={pathname.includes(link.href)}
+                  href={link.href}
+                >
+                  {link.label}
+                </StyledLink>
               </li>
             ))}
           </ol>
