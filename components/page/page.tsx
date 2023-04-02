0753eb9da73e8d883906d2fec0141274c31da5fd
Niklas Albinsson <albinssonniklas@gmail.com>
2023-04-02 14:20:40 +0200

    Fix width issue on mobile

--- components/page/page.tsx
+++ components/page/page.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
 const Page: FC = ({ children }): JSX.Element => (
-  <main className="relative mx-auto max-w-2xl space-y-20 px-4 py-6 sm:pt-28 md:px-0 2xl:space-y-32">
+  <main className="relative mx-auto w-full max-w-2xl space-y-20 px-4 py-6 sm:pt-28 md:px-0 2xl:space-y-32">
     {children}
   </main>
 );
