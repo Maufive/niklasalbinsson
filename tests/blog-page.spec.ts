4882d69e85b6a16841178c44d436552d94edadc6
Niklas Albinsson <albinssonniklas@gmail.com>
2023-08-30 20:48:30 +0200

    Update paths in tests

--- tests/blog-page.spec.ts
+++ tests/blog-page.spec.ts
@@ -3,14 +3,14 @@ import { test, expect } from '@playwright/test';
 test('navigation to a blog post through the blog page and back', async ({
   page,
 }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('/');
   await page.getByRole('link', { name: 'Blog', exact: true }).click();
 
-  await page.goto('http://localhost:3000/blog');
+  await page.goto('/blog');
   await expect(page.getByRole('heading', { name: 'All posts' })).toBeVisible();
 
   await page.getByTitle('React Server Components').click();
-  await page.goto('http://localhost:3000/blog/react-server-components');
+  await page.goto('/blog/react-server-components');
 
   await expect(
     page.getByRole('heading', { name: 'React Server Components' })
--- tests/featured-project.spec.ts
+++ tests/featured-project.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('navigation to the featured project', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('/');
 
   await expect(page.getByTestId('featured-project-link')).toBeVisible();
   await expect(
@@ -9,7 +9,7 @@ test('navigation to the featured project', async ({ page }) => {
   ).toBeVisible();
   await page.getByTestId('featured-project-link').click();
 
-  await page.goto('http://localhost:3000/projects/bookmarked');
+  await page.goto('/projects/bookmarked');
 
   await expect(page.getByRole('img', { name: 'Project Image' })).toBeVisible();
   await expect(page.getByRole('link', { name: 'View Project' })).toBeVisible();
