d8e2f48265d0018c8734df29e78423718e3e7550
Niklas Albinsson <albinssonniklas@gmail.com>
2024-02-19 20:04:15 +0100

    WIP Macos dock

---
+++ app/playground/page.tsx
@@ -0,0 +1,22 @@
+import Page from 'components/page';
+
+// https://github.com/ysj151215/big-sur-dock/blob/main/types/index.ts
+
+const GRADIENTS = [
+    'https://products.ls.graphics/mesh-gradients/images/03.-Snowy-Mint_1-p-130x130q80.jpeg',
+    'https://products.ls.graphics/mesh-gradients/images/04.-Hopbush_1-p-130x130q80.jpeg',
+    'https://products.ls.graphics/mesh-gradients/images/06.-Wisteria-p-130x130q80.jpeg',
+    'https://products.ls.graphics/mesh-gradients/images/09.-Light-Sky-Blue-p-130x130q80.jpeg',
+    'https://products.ls.graphics/mesh-gradients/images/12.-Tumbleweed-p-130x130q80.jpeg',
+    'https://products.ls.graphics/mesh-gradients/images/15.-Perfume_1-p-130x130q80.jpeg',
+    null,
+    'https://products.ls.graphics/mesh-gradients/images/36.-Pale-Chestnut-p-130x130q80.jpeg',
+  ]
+
+export default function Projects() {
+  return (
+    <Page>
+        
+    </Page>
+  );
+}
